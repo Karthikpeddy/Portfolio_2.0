4f0c404c4932bdb1cccd12d8a3ed1f3f87ca4e9e
karthik peddy <117649571+Karthikpeddy@users.noreply.github.com>
2024-03-01 18:47:39 +0000

    navbar_2.0

---
+++ src/Test.jsx
@@ -0,0 +1,12 @@
+const Test=()=>{
+
+    return( 
+        <div className="course">
+            <div className="box">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Test;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import "./Navbar.scss"
+import Sidebar from "./sidebar/Sidebar.jsx"
+import { motion } from "framer-motion";
 
 const Navbar=()=>{
 
     return( 
         <div className="navbar">
+            <Sidebar/>
             <div className="wrapper">
-                <span>Karthik Peddy</span>
+                <motion.span initial={{opacity:0 , scale:0.5}}
+                animate={{opacity:1 , scale:1}}
+                transition={{duration:0.5}}
+                >Karthik Peddy</motion.span>
                 <div className="social">
                     <a href="#">
                         <img src="./facebook.png" alt="" />
